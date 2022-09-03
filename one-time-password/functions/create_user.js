d9946fc86c2fc5030757c6dd315890ffbfb41136
Pablo <pablo.medina@the-cocktail.com>
2022-09-03 15:54:57 +0200

    feat(OTP): Create user firebase cloud function

--- one-time-password/functions/create_user.js
+++ one-time-password/functions/create_user.js
@@ -1,18 +1,16 @@
-const admin = require('firebase-admin');
+const admin = require("firebase-admin");
 
-module.exports = function(req, res) {
+module.exports = (req, res) => {
   // Verify the user provided a phone
-  if (!req.body.phone) {
-    return res.status(422).send({ error: 'Bad Input' });
-  }
+  if (!req.body.phone) return res.status(422).send({ error: "Bad Input" });
 
   // Format the phone number to remove dashes and parens
   const phone = String(req.body.phone).replace(/[^\d]/g, "");
 
   // Create a new user account using that phone number
-  admin.auth().createUser({ uid: phone })
-    .then(user => res.send(user))
-    .catch(err => res.status(422).send({ error: err }));
-
-  // Respond to the user request, saying the account was made
-}
+  admin
+    .auth()
+    .createUser({ uid: phone })
+    .then((user) => res.send(user))
+    .catch((error) => res.status(422).send({ error }));
+};
--- one-time-password/functions/index.js
+++ one-time-password/functions/index.js
@@ -1,15 +1,4 @@
-const admin = require('firebase-admin');
-const functions = require('firebase-functions');
-const createUser = require('./create_user');
-const serviceAccount = require('./service_account.json');
-const requestOneTimePassword = require('./request_one_time_password');
-const verifyOneTimePassword = require('./verify_one_time_password');
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://one-time-password-c725a.firebaseio.com"
-});
+const functions = require("firebase-functions");
+const createUser = require("./create_user");
 
 exports.createUser = functions.https.onRequest(createUser);
-exports.requestOneTimePassword = functions.https.onRequest(requestOneTimePassword);
-exports.verifyOneTimePassword = functions.https.onRequest(verifyOneTimePassword);
